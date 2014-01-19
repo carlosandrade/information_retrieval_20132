a36f51e7fe2793bd21c4636bbe5be31f78b1315c
Victor <vitocr_santos@hotmail.com>
2014-01-19 20:12:58 -0300

    Removing not needed settings

--- equipe_1/crawler/settings.py
+++ equipe_1/crawler/settings.py
@@ -12,10 +12,6 @@ SPIDER_MODULES = ['crawler.spiders']
 NEWSPIDER_MODULE = 'crawler.spiders'
 COOKIES_ENABLED = False
 DOWNLOAD_DELAY = 2
-FEED_FORMAT = 'json'
-FEED_URI = 'file:///tmp/export.jso'
-
-# DEPTH_LIMIT = 1
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
+# USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
