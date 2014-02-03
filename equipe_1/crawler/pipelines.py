46ad58b5cd5fcc9612828475bc1d38a873ce11cd
Victor <vitocr_santos@hotmail.com>
2014-02-03 01:20:09 -0300

    Inserindo pipeline

--- equipe_1/crawler/pipelines.py
+++ equipe_1/crawler/pipelines.py
@@ -6,3 +6,15 @@
 class TutorialPipeline(object):
     def process_item(self, item, spider):
         return item
+
+class CleanPipeline(object):
+	def process_item(self, item, spider):
+		# Obtem conjunto de stopwords em portugues, e as armazena
+		stopset = set(stopwords.words('portuguese'))
+		if item['nome']:
+			tokens = nltk.word_tokenize(item['nome'])    
+		elif item['program']:
+			tokens = nltk.word_tokenize(item['program'])    
+		# Adiciona o token ao texto se o mesmo nao for uma stopword e possui tamanho maior que 2
+		clean = [token for token in tokens if not token in stopset and len(token) > 2]
+		return clean
